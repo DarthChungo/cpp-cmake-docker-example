9719135d931506445eb385de6b2ccc0080e57eda
DarthChungo <antonio@adaro.eu>
2022-08-07 14:57:42 +0200

    Initial CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "include/test.hpp"
+
+int main() {
+  test::test();
+  return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "include/test.hpp"
+
+namespace test {
+void test() {
+  std::cout << "This is a test\n";
+}
+}
